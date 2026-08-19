24861c778624833c4b5b4065ecebc44748d438f4
Andrey <andreygabriellins15@gmail.com>
2026-08-18 21:34:23 -0300

    Modificações no projeto do Jogo da Forca feito em Java: fix: método progressoPalavra foi corrigido, sendo colocado corretamente fora do while; add: criação do método isVitoria para verificar se o player acertou uma letra da palavra sorteada add: criação do método fimDeJogo, colocando toda a estrutura sem as devidas correções para o funcionamento

--- src/entities/JogoForca.java
+++ src/entities/JogoForca.java
@@ -14,52 +14,60 @@ public class JogoForca {
 
     public JogoForca(String palavra) {
         this.palavra = palavra;
-        boolean[] revelado = new boolean[palavra.length()];
+        revelado = new boolean[palavra.length()];
 
         while (vidas > 0 && !vitoria) {
             System.out.println("\nVidas: " + vidas);
             if (!erradas.isEmpty()) {
                 System.out.println("Erradas: " + erradas);
             }
-            public String progressoPalavra(){
 
-                int i = 0;
-                String resposta = "";
-                while (i < palavra.length()) {
-                    if (revelado[i]) {
-                        resposta += palavra.charAt(i);
-                    } else {
-                        resposta += "_";
-                    }
-                    i++;
-                }
-                return resposta;
-            }
-
-            if (acertos == palavra.length()) {
-                vitoria = true;
-                break;
-            }
             System.out.println("\nDigite uma letra: ");
             String entrada = sc.next().toLowerCase();
             char letra = entrada.charAt(0);
+        }
+    }
+    public String progressoPalavra(){
+        int i = 0;
+        String resposta = "";
+        while (i < palavra.length()) {
+            if (revelado[i]) {
+                resposta += palavra.charAt(i);
+            } else {
+                resposta += "_";
+            }
+            i++;
+        }
+        return resposta;
+    }
 
-            boolean achou = false;
-            int j = 0;
-            while (j < palavra.length()) {
-                if (palavra.charAt(j) == letra) {
-                    revelado[j] = true;
-                    achou = true;
-                }
-                j++;
+    public boolean isVitoria() {
+        boolean correta = true;
+        int i = 0;
+        while (i < revelado.length) {
+            if(revelado[i] == false) {
+                correta = false;
             }
-            if (!achou && erradas.indexOf(letra) == -1) {
-                erradas += letra + " ";
-                vidas--;
-                System.out.println(letra + " não está na palavra");
-            } else if (achou) {
-                System.out.println(letra + " está na palavra");
+            i++;
+        }
+        return correta;
+    }
+    public void fimDeJogo() {
+        boolean achou = false;
+        int j = 0;
+        while (j < palavra.length()) {
+            if (palavra.charAt(j) == letra) {
+                revelado[j] = true;
+                achou = true;
             }
+            j++;
+        }
+        if (!achou && erradas.indexOf(letra) == -1) {
+            erradas += letra + " ";
+            vidas--;
+            System.out.println(letra + " não está na palavra");
+        } else if (achou) {
+            System.out.println(letra + " está na palavra");
         }
         if (vitoria) {
             System.out.println("Você venceu! A palavra era: " + palavra);
